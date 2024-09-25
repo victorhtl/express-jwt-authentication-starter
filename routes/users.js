b2ddc09b7be8b2e426b68ccc57a0cbb96d968c6d
victorhtl <vht.luna@gmail.com>
2024-09-25 14:50:46 -0300

    login jwt works

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const fs = require('fs')
 const path = require('path');
-const jwt = require('jwt-simple')
+const jwt = require('jwt-simple');
 
 const pathToKey = path.join(__dirname, '..', 'id_rsa_priv.pem');
 const PRIV_KEY = fs.readFileSync(pathToKey, 'utf8');
@@ -13,7 +13,38 @@ const PRIV_KEY = fs.readFileSync(pathToKey, 'utf8');
 router.get('/protected', (req, res, next) => {
 });
 
-router.post('/login', (req, res, next)=>{});
+function issueJWT(user){
+    const now = Math.floor(Date.now() / 1000);
+
+    const payload = {
+        sub: user._id,
+        name: user.username,
+        iat: now,
+        exp: now + (60 * 60 * 24)
+    };
+
+    return {
+        payload,
+        token: jwt.encode(payload, PRIV_KEY)
+    };
+}
+
+router.post('/login', (req, res, next)=>{
+    User.findOne({username: req.body.username})
+        .then(user => {
+            if (!user) return res.status(401).json({error: 'Invalid username or password'});
+
+            bcrypt.compare(req.body.password, user.password)
+               .then(isMatch => {
+                    if (!isMatch) return res.status(401).json({error: 'Invalid username or password'});
+
+                    const jwt = issueJWT(user);
+
+                    res.json({...jwt.payload, token: jwt.token});
+                })
+               .catch(err => res.status(500).send(err))
+        })
+});
 
 router.post('/register', async (req, res, next) => {
     const hash_password = await bcrypt.hash(req.body.password, 10);
@@ -25,19 +56,8 @@ router.post('/register', async (req, res, next) => {
 
     newUser.save()
         .then( user => {
-            const now = Math.floor(Date.now() / 1000)
-
-            const payload = {
-                sub: user._id,
-                name: user.username,
-                iat: now,
-                exp: now + (60 * 60 * 24) // 1 day
-            };
-
-            res.json({
-                ...payload,
-                token: jwt.encode(payload, PRIV_KEY)
-            })
+            const jwt = issueJWT(user);
+            res.json({...jwt.payload, token: jwt.token})
         })
         .catch(err => res.status(500).send(err))
 });
